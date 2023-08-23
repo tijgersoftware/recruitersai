73d19cb5186f406d4b4a0b6efaecc77080af8502
tijgersoftware <tijgersoftware@gmail.com>
2023-08-23 23:39:28 +0200

    scss and big screens

--- js/logic.js
+++ js/logic.js
@@ -49,3 +49,40 @@ document.addEventListener("DOMContentLoaded", function () {
     revealContent(hiddenContent); // Call the function after 8 seconds
   }, 12000);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  function setActiveTab(tabId, tabs) {
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+
+    const tab = document.getElementById(tabId);
+    if (tab) {
+      tab.classList.add("active");
+    }
+  }
+
+  setTimeout(function () {
+    const tabs = document.querySelectorAll(".tab");
+    console.log(tabs);
+
+    // Check the URL to determine the active tab
+    const url = window.location.href;
+    if (
+      url.endsWith("index.html") ||
+      url.endsWith("/") ||
+      url.endsWith("5500") ||
+      url.endsWith(".com")
+    ) {
+      setActiveTab("home-tab", tabs);
+    } else if (url.endsWith("features.html")) {
+      setActiveTab("features-tab", tabs);
+    } else if (url.endsWith("pricing.html")) {
+      setActiveTab("pricing-tab", tabs);
+    } else if (url.endsWith("company.html")) {
+      setActiveTab("company-tab", tabs);
+    } else if (url.endsWith("info.html")) {
+      setActiveTab("services-tab", tabs);
+    }
+  }, 300); // 5000 milliseconds = 5 seconds
+});
